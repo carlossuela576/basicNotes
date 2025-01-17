4fbdecc548772aa0cc14462f83779f40aeb9408f
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-01-17 17:06:57 +0700

    update commit
    - use of two 1d arrays for precise value handling.
    - for loop array initialization update.
    - better prompts

--- basicNotes.java
+++ basicNotes.java
@@ -8,23 +8,27 @@ public class basicNotes {
         System.out.println("Welcome to Basic Notes!");
         //user input for the number of notes
         System.out.print("How many items would you like to enter?: ");
-        int rowItems = sc.nextInt();
+        int columnIt = sc.nextInt();
 
         //error occurs without this, consumes the next line
         //without this the program will skip the first input
         sc.nextLine();
 
-        //initialize dynamic rows
-        String[][] notes = new String[rowItems][2];
+        //initialize dynamic column
+        String [] title = new String[columnIt];
+        String [] notes = new String[columnIt];
         //prompt user to enter their notes
         System.out.println("Please enter your notes below.");
 
         //loop array initialization
-        for (int i = 0; i < notes.length; i++){
-            for (int j = 0; j < notes[i].length; j++){
-                System.out.println("Enter note " + (i + 1) + ": ");
-                notes[i][j] = sc.nextLine();
-            }
+        for (int i = 0; i < columnIt; i++){
+            System.out.println("Enter title");
+            System.out.print((i+1) + ". ");
+            title[i] = sc.nextLine().toUpperCase();
+
+            System.out.println("Enter note");
+            System.out.print((i+1) + ". ");
+            notes[i] = sc.nextLine();
         }
 
         //loop through the array and print the notes
@@ -32,11 +36,11 @@ public class basicNotes {
         System.out.println("HERE ARE THE NOTES YOU'VE ENTERED");
         System.out.println("-----------------------------------");
 
-        for (int row = 0; row < notes.length; row++) {
+        for (int row = 0; row < columnIt; row++){
+            System.out.println("note " + (row+1));
+            System.out.println(title[row]);
+            System.out.println(notes[row]);
             System.out.println();
-            for (int colu = 0; colu < notes[row].length; colu++) {
-                System.out.println(notes[row][colu] + " ");
-            }
         }
     }
 }
